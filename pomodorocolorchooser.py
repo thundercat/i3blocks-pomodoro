e60a77db7258f76ba2cac3637c5808b6a6c2b020
Krzysiek <wloszekk@gmail.com>
2019-09-28 15:20:13 +0200

    translate to english

--- pomodorocolorchooser.py
+++ pomodorocolorchooser.py
@@ -4,9 +4,9 @@ from PyQt5.QtGui import QColor
 from PyQt5.QtCore import pyqtSlot
 
 
-class MyButton(QPushButton):
+class ColorQButton(QPushButton):
     def __init__(self, parent, index, dic_key):
-        super(MyButton, self).__init__(parent)
+        super(ColorQButton, self).__init__(parent)
         self.index = index
         self.dic_key = dic_key
 
@@ -29,13 +29,13 @@ class PomodoroColorChooser(QWidget):
         self.button_list = list()
         self.interface()
 
-    def initButtons(self, label_str, dic_key, cfg=None):
+    def init_buttons(self, label_str, dic_key, cfg=None):
         layout_h = QHBoxLayout()
         label = QLabel(label_str)
         layout_h.addWidget(label)
 
         for index in range(0, len(cfg[dic_key])):
-            bt = MyButton(self, index, dic_key)
+            bt = ColorQButton(self, index, dic_key)
             bt.setStyleSheet("background-color:{};".format(cfg[dic_key][index][1]))
             bt.color = cfg[dic_key][index][1]
             layout_h.addWidget(bt)
@@ -56,28 +56,26 @@ class PomodoroColorChooser(QWidget):
         self.close()
 
     def interface(self):
-        # przypisanie widgetów do układu tabelarycznego
         layout = QGridLayout()
 
-        koniecBtn = QPushButton("&Koniec && Zapis", self)
-        koniecBtn.resize(koniecBtn.sizeHint())
-        koniecBtn.clicked.connect(self.exit_and_save)
+        quit_button = QPushButton("&Quit && Save", self)
+        quit_button.resize(quit_button.sizeHint())
+        quit_button.clicked.connect(self.exit_and_save)
 
-        saveBtn = QPushButton("Zapis", self)
-        saveBtn.resize(saveBtn.sizeHint())
-        saveBtn.clicked.connect(self.save_colors)
+        save_button = QPushButton("Save", self)
+        save_button.resize(save_button.sizeHint())
+        save_button.clicked.connect(self.save_colors)
 
-        layout.addLayout(self.initButtons("Praca", "WORKING_COLOR", self.cfg), 1, 0, 1, 3)
-        layout.addLayout(self.initButtons("Przerwa", "NORMAL_BREAK", self.cfg), 2, 0, 1, 3)
-        layout.addLayout(self.initButtons("Długa przerwa", "LONG_BREAK", self.cfg), 3, 0, 1, 3)
+        layout.addLayout(self.init_buttons("Pomodoro", "WORKING_COLOR", self.cfg), 1, 0, 1, 3)
+        layout.addLayout(self.init_buttons("Break", "NORMAL_BREAK", self.cfg), 2, 0, 1, 3)
+        layout.addLayout(self.init_buttons("Long break", "LONG_BREAK", self.cfg), 3, 0, 1, 3)
 
-        layout.addWidget(saveBtn, 4, 0, 1, 3)
-        layout.addWidget(koniecBtn, 5, 0, 1, 3)
+        layout.addWidget(save_button, 4, 0, 1, 3)
+        layout.addWidget(quit_button, 5, 0, 1, 3)
 
-        # przypisanie utworzonego układu do okna
         self.setLayout(layout)
         self.resize(300, 100)
-        self.setWindowTitle("pomodorocolorchooser.py")
+        self.setWindowTitle("pomodorocolorchooser")
         self.show()
 
 
